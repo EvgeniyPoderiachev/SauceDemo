1a4d14f4438c8aa44709c9b819dd19f5bcf9e47b
Eugene <setminsk@gmail.com>
2022-03-27 20:37:38 +0300

    TMS-8-added-checkoutTest

---
+++ src/test/java/CheckoutTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CheckoutTest extends BaseTest {
+
+
+    @Test
+    public void checkout() {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.cssSelector("input[placeholder=Username]")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.name("login-button")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.id("shopping_cart_container")).click();
+        driver.findElement(By.cssSelector("#checkout")).click();
+        driver.findElement(By.cssSelector("#first-name")).sendKeys("Evgen");
+        driver.findElement(By.cssSelector("#last-name")).sendKeys("Poderyachev");
+        driver.findElement(By.cssSelector("#postal-code")).sendKeys("1234");
+        driver.findElement(By.name("continue")).click();
+        driver.findElement(By.name("finish")).click();
+        String text = driver.findElement(By.xpath("//h2[text()='THANK YOU FOR YOUR ORDER']")).getText();
+        assertEquals(text, "THANK YOU FOR YOUR ORDER", "msg");
+
+    }
+}
