57c64ebfd3dbfcf3e16dc2584c2f4597ea37c592
Eugene <setminsk@gmail.com>
2022-04-14 13:50:11 +0300

    TMS-8-added-Tests

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -37,7 +37,8 @@ public class BaseTest {
 
     @AfterMethod(alwaysRun = true)
     public void close() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
-
 }
